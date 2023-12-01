6f9f93fd3c013c72b44df95c99642bb5ff088931
Osama Saeed <osama.saeed@shield.ai>
2023-12-01 03:07:07 -0800

    init and day 1

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+	"github.com/0samaS/AdventOfCode2023/sols"
+	"github.com/fatih/color"
+	"os"
+)
+
+func main() {
+	part := os.Args[1]
+	yellow := color.New(color.FgYellow)
+	yellow.Printf("Solution for part %s is: %d\n", part, sols.Day1(part))
+}
---
+++ sols/day1.go
@@ -0,0 +1,80 @@
+package sols
+
+import (
+	"bufio"
+	"os"
+	"unicode"
+	"regexp"
+)
+
+func part1() int {
+	res := 0
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		var first, current int = 0, 0
+		var found_first = false
+		for _, c := range line {
+			if unicode.IsDigit(c){
+				current = int(c - '0')
+				if !found_first{
+					first = current
+					found_first = true
+				}
+			}
+		}
+		res += (first*10 + current)
+	}
+
+	return res
+}
+
+func Reverse(s string) (result string) {
+  for _,v := range s {
+    result = string(v) + result
+  }
+  return
+}
+
+func part2() int {
+	digitsMap := map[string]int{
+		"one": 1,
+		"two": 2,
+		"three": 3,
+		"four": 4,
+		"five": 5,
+		"six": 6,
+		"seven": 7,
+		"eight": 8,
+		"nine": 9,
+		"1": 1,
+		"2": 2,
+		"3": 3,
+		"4": 4,
+		"5": 5,
+		"6": 6,
+		"7": 7,
+		"8": 8,
+		"9": 9,
+	}
+	forward_re := regexp.MustCompile(`(\d|one|two|three|four|five|six|seven|eight|nine)`)
+	back_re := regexp.MustCompile(`(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)`)
+	res := 0
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		forward_matches := forward_re.FindAllString(line, -1)
+		back_matches := back_re.FindAllString(Reverse(line), -1)
+		res += (digitsMap[forward_matches[0]]*10 + digitsMap[Reverse(back_matches[0])])
+	}
+	return res
+}
+
+func Day1(part string) int {
+	if part == "1" {
+		return part1()
+	} else {
+		return part2()
+	}
+}
