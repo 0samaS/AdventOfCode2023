a141f4f3c30e8882fc2a16b7914eff79f4993d85
Osama Saeed <osama.saeed@shield.ai>
2023-12-04 10:09:02 -0800

    day 4 part 1

--- main.go
+++ main.go
@@ -9,5 +9,5 @@ import(
 func main() {
 	part := os.Args[1]
 	yellow := color.New(color.FgYellow)
-	yellow.Printf("Solution for part %s is: %d\n", part, sols.Day3(part))
+	yellow.Printf("Solution for part %s is: %d\n", part, sols.Day4(part))
 }
--- sols/day4.go
+++ sols/day4.go
@@ -1,11 +1,50 @@
 package sols
 
 import (
+	"bufio"
+	"os"
+	"strings"
+	// "fmt"
+	"strconv"
+	"math"
 )
 
 func part1Day4() int {
 	res := 0
 
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		split_card := strings.Split(line, ":")
+		split_card = strings.Split(split_card[1], "|")
+
+		winning_nums := strings.Fields(strings.TrimSpace(split_card[0]))
+		our_nums := strings.Fields(strings.TrimSpace(split_card[1]))
+
+		our_nums_map := make(map[int]int)
+		winning_nums_map := make(map[int]int)
+
+		for _, num_str := range our_nums {
+			num, _ := strconv.Atoi(num_str)
+			our_nums_map[num] += 1
+		}
+		for _, num_str := range winning_nums {
+			num, _ := strconv.Atoi(num_str)
+
+			winning_nums_map[num] += 1
+		}
+
+		// check if winning numbers appear in our set
+		num_in_common := 0
+		for num, _ := range our_nums_map {
+
+			_, exists := winning_nums_map[num]
+			if exists {
+				num_in_common += 1
+			}
+		}
+		res += int(math.Pow(2, float64(num_in_common - 1)))
+	}
 	return res
 }
 
