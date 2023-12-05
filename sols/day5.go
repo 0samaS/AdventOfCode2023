a618908e33310328f6c694272d5bb43ffa604710
Osama Saeed <osama.saeed@shield.ai>
2023-12-04 23:32:44 -0800

    linear search from location 0, optimized solution

--- sols/day5.go
+++ sols/day5.go
@@ -5,14 +5,33 @@ import (
 	"os"
 	"strings"
 	"strconv"
-	"fmt"
 	"math"
 )
 
 type RangeType struct { 
 	start int
 	range_len int
-} 
+}
+
+func translatableToSeed(map_of_maps map[string] map[RangeType]RangeType, mapping_key_list []string, seeds_list []RangeType, tester int) bool {
+	intermediate := tester
+	for i:=len(mapping_key_list)-1; i >= 0; i-- {
+		key := mapping_key_list[i]
+		for src_info, dest_info:= range map_of_maps[key] {
+			// this can be translated
+			if intermediate >= src_info.start && intermediate < src_info.start + src_info.range_len {
+				intermediate = (intermediate - src_info.start + dest_info.start)
+				break
+			}
+		}
+	}
+	for _, seed_info := range seeds_list {
+		if intermediate > seed_info.start && intermediate < seed_info.start + seed_info.range_len {
+			return true
+		}
+	}
+	return false
+}
 
 func part1Day5() int {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -43,6 +62,7 @@ func part1Day5() int {
 			map_key_name = split_mapping_names[0]
 			mapping_key_list = append(mapping_key_list, map_key_name)
 			map_of_maps[map_key_name] = make(map[RangeType]RangeType)
+
 			done_with_current_mapping = false
 			continue
 		}
@@ -98,16 +118,13 @@ func part2Day5() int {
 			continue
 		}
 		if done_with_current_mapping {
-			// fmt.Printf("%s\n", line)
-			if line_items[1] != "map:" {
-				fmt.Printf("Something has gone terribly wrong!")
-			}
 			split_mapping_names := strings.Split(line_items[0], "-")
 
-			map_key_name = split_mapping_names[0]
+			// we go backwards now
+			map_key_name = split_mapping_names[2]
 			mapping_key_list = append(mapping_key_list, map_key_name)
 			map_of_maps[map_key_name] = make(map[RangeType]RangeType)
-			// fmt.Printf("\n\n%s key name %d \n", map_key_name, len(map_of_maps))
+
 			done_with_current_mapping = false
 			continue
 		}
@@ -115,31 +132,16 @@ func part2Day5() int {
 		src_start, _ := strconv.Atoi(line_items[1])
 		range_len, _ := strconv.Atoi(line_items[2])
 
-		map_of_maps[map_key_name][RangeType{src_start, range_len}] = RangeType{dest_start, range_len}
+		// switch dest and src around
+		map_of_maps[map_key_name][RangeType{dest_start, range_len}] = RangeType{src_start, range_len}
 	}
-	min_location := math.MaxInt32
 
-	for _, seed_info := range seeds_list {
-		// fmt.Printf("%d, %d\n", seed_info.start, seed_info.range_len)
-		for seed:=seed_info.start; seed < seed_info.start + seed_info.range_len; seed++ {
-			intermediate := seed
-			for _, key := range mapping_key_list {
-				for src_info, dest_info:= range map_of_maps[key] {
-					// this can be translated
-					if intermediate >= src_info.start && intermediate < src_info.start + src_info.range_len {
-						intermediate = (intermediate - src_info.start + dest_info.start)
-						break
-					}
-				}
-			}
-			if intermediate < min_location {
-				min_location = intermediate
-			}
-		}
+	location := 0
+	for !translatableToSeed(map_of_maps, mapping_key_list, seeds_list, location) {
+		location++;
+	}
 
-	}	
-
-	return min_location
+	return location
 }
 
 func Day5(part string) int {
