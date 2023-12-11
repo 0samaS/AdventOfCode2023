e0701d3e9a878962fa40de0ef3b09783140b4a87
Osama Saeed <osama.saeed@shield.ai>
2023-12-10 23:44:08 -0800

    refactor and part 2

--- sols/day11.go
+++ sols/day11.go
@@ -7,29 +7,7 @@ import (
 	// "fmt"
 	// "strings"
 	// "strconv"
-	"regexp"
 )
-
-// var dist_map = map[]
-
-// func dist(v1 int, v2 int, u1 int, u2 int) {
-
-// }
-
-// func APSP(expanded_universe []string) {
-// 	var dist []
-// 	for v1:=0; v1<len(expanded_universe); v1++ {
-// 		for u1:=0; u1<len(expanded_universe); u1++ {
-// 			for v2:=0; v2<len(expanded_universe); v2++ {
-// 				for u2:=0; u2<len(expanded_universe); u2++ {
-// 					if v1 == u1 && v2 == u2 {
-// 						dist[Node{v1,}]
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// }
 
 // Abs returns the absolute value of x.
 func abs(x int) int {
@@ -53,6 +31,50 @@ func findManhattanDist(x Galaxy, y Galaxy) int {
 	return abs(x.row - y.row) + abs(x.col - y.col)
 }
 
+func findExpandedManhattanDist(x Galaxy, y Galaxy, row_info[]int, col_info[]int, len_expansion int) int {
+	min_row := 0
+	max_row := 0
+	min_col := 0
+	max_col := 0
+	if x.row < y.row {
+		min_row = x.row
+		max_row = y.row
+
+	} else if y.row < x.row {
+		min_row = y.row
+		max_row = x.row
+	}
+	if x.col < y.col {
+		min_col = x.col
+		max_col = y.col
+
+	} else if y.col < x.col {
+		min_col = y.col
+		max_col = x.col
+	}
+
+	row_dist := 0
+	for min_row < max_row {
+		if row_info[min_row] == 0 {
+			row_dist += len_expansion
+		} else {
+			row_dist += 1
+		}
+		min_row++
+	}
+
+	col_dist := 0
+	for min_col < max_col {
+		if col_info[min_col] == 0 {
+			col_dist += len_expansion
+		} else {
+			col_dist += 1
+		}
+		min_col++
+	}
+	return row_dist + col_dist
+}
+
 func part1Day11() int {
 	res := 0
 	var universe []string
@@ -63,68 +85,75 @@ func part1Day11() int {
 		universe = append(universe, line)
 	}
 	col_info := make([]int, len(universe[0]))
+	row_info := make([]int, len(universe))
 	for i:=0; i<len(universe); i++ {
 		for j:=0; j<len(universe[i]); j++ {
 			if universe[i][j] == '#' {
 				col_info[j] += 1
+				row_info[i] += 1
 			} else {
 				col_info[j] += 0
+				row_info[i] += 0
 			}
 		}
 	}
-
-	var expanded_universe []string
-	re := regexp.MustCompile(`#`)
-
-	for _, row := range universe {
-		new_row := row
-		buffer_col := 0
-
-		for col_num, num_galaxies := range col_info {
-
-			if num_galaxies == 0 {
-				new_row = new_row[0:col_num+buffer_col] + string('.') + new_row[col_num+buffer_col:len(new_row)]
-				buffer_col++
-			}
-		}
-		expanded_universe = append(expanded_universe, new_row)
-		matches := re.FindString(new_row)
-		// add extra row if empty
-		if len(matches) == 0 {
-			expanded_universe = append(expanded_universe, new_row)
-		}
-	}
-
 	var galaxies []Galaxy
-
-	for row_num, line := range expanded_universe {
+	for row_num, line := range universe {
 		for col_num, char := range line {
 			if char == '#' {
 				galaxies = append(galaxies, Galaxy{row_num, col_num})
 			}
 		}
 	}
-
 	dist_map := make(map[GalaxyPair]int)
 	for i:=0; i<len(galaxies); i++ {
 		for j:=i+1; j<len(galaxies); j++ {
 			galaxy_pair := GalaxyPair{i, j}
-			dist_map[galaxy_pair] = findManhattanDist(galaxies[i], galaxies[j])
+			dist_map[galaxy_pair] = findExpandedManhattanDist(galaxies[i], galaxies[j], row_info, col_info, 2)
 			res += dist_map[GalaxyPair{i, j}]
 		}
 	}
-
 	return res
 }
 
 func part2Day11() int {
 	res := 0
+	var universe []string
+	scanner := bufio.NewScanner(os.Stdin)
 	
-	// scanner := bufio.NewScanner(os.Stdin)
-	// for scanner.Scan() {
-		// line := scanner.Text()
-	// }
-
+	for scanner.Scan() {
+		line := scanner.Text()
+		universe = append(universe, line)
+	}
+	col_info := make([]int, len(universe[0]))
+	row_info := make([]int, len(universe))
+	for i:=0; i<len(universe); i++ {
+		for j:=0; j<len(universe[i]); j++ {
+			if universe[i][j] == '#' {
+				col_info[j] += 1
+				row_info[i] += 1
+			} else {
+				col_info[j] += 0
+				row_info[i] += 0
+			}
+		}
+	}
+	var galaxies []Galaxy
+	for row_num, line := range universe {
+		for col_num, char := range line {
+			if char == '#' {
+				galaxies = append(galaxies, Galaxy{row_num, col_num})
+			}
+		}
+	}
+	dist_map := make(map[GalaxyPair]int)
+	for i:=0; i<len(galaxies); i++ {
+		for j:=i+1; j<len(galaxies); j++ {
+			galaxy_pair := GalaxyPair{i, j}
+			dist_map[galaxy_pair] = findExpandedManhattanDist(galaxies[i], galaxies[j], row_info, col_info, 1000000)
+			res += dist_map[GalaxyPair{i, j}]
+		}
+	}
 	return res
 }
 
